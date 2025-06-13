dbee53417accf722764bf5f66c79cc7d7b852a60
guotai <guotaixiao87@gmail.com>
2025-06-14 09:43:06 +1000

    fix: convert the lat and lng from string to number, since goole map api require number

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,13 +1,13 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from "react";
 
-import Card from '../../shared/components/UIElements/Card';
-import Button from '../../shared/components/FormElements/Button';
-import Modal from '../../shared/components/UIElements/Modal';
-import Map from '../../shared/components/UIElements/Map';
-import { AuthContext } from '../../shared/context/auth-context';
-import './PlaceItem.css';
+import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
+import { AuthContext } from "../../shared/context/auth-context";
+import "./PlaceItem.css";
 
-const PlaceItem = props => {
+const PlaceItem = (props) => {
   const auth = useContext(AuthContext);
   const [showMap, setShowMap] = useState(false);
   const [showConfirmModal, setShowConfirmModal] = useState(false);
@@ -26,7 +26,7 @@ const PlaceItem = props => {
 
   const confirmDeleteHandler = () => {
     setShowConfirmModal(false);
-    console.log('DELETING...');
+    console.log("DELETING...");
   };
 
   return (
@@ -40,7 +40,13 @@ const PlaceItem = props => {
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
         <div className="map-container">
-          <Map center={props.coordinates} zoom={16} />
+          <Map
+            center={{
+              lat: Number(props.coordinates.lat),
+              lng: Number(props.coordinates.lng),
+            }}
+            zoom={16}
+          />
         </div>
       </Modal>
       <Modal
